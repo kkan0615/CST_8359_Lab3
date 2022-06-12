957a42bb9f595a981e2a997082c7231a9f8cd145
unknown <kkan0615@gmail.com>
2022-06-11 22:24:55 -0400

    :rocket: version 0.0.3

---
+++ Lab3/Controllers/Home/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            System.Diagnostics.Debug.WriteLine(Request.Form["numberOfBottles"]);
+            HttpContext.Session.SetString("numberOfBottles", Request.Form["numberOfBottles"]);
+            // you will complete this
+            return View();
+        }
+
+        public IActionResult CreatePerson() => View();
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            // you will complete this
+            return View(person);
+
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab3/Models/Person.cs
@@ -0,0 +1,41 @@
+﻿namespace Lab3.Models
+{
+    public class Person
+    {
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
